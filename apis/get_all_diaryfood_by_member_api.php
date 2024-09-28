8e0e9bda1d0c73b81e1784cc1dcbfabfb41d6e04
Adisorn <s6552410001@sau.ac.th>
2024-09-28 17:39:08 +0700

    first commit

---
+++ apis/get_all_diaryfood_by_member_api.php
@@ -0,0 +1,54 @@
+<?php //get_all_diaryfood_by_member_api.php ดึงข้อมูลเฉพาะข้อมูลการกินของสมาชิกคนนั้นๆเท่านั้น
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET"); //POST, PUT, DELETE
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once "./../connectdb.php";
+require_once "./../models/diaryfood.php";
+
+//สร้าง Instance (Object/ตัวแทน)
+$connDB = new ConnectDB();
+$diaryfood = new Diaryfood($connDB->getConnectionDB());
+
+$data = json_decode(file_get_contents("php://input"));
+
+$diaryfood->memId = $data->memId;
+//เรียกใช้ฟังก์ชันดึงข้อมูลทั้งหมดจากตาราง diaryfood_tb
+$result = $diaryfood->getAllDiaryfoodByMemId();
+
+//ตรวจสอบข้อมูลจากการเรัยกใช้ฟังก์ชันตรวจสอบชื่อผู้ใช้ รหัสผ่าน
+if ($result->rowCount() > 0) {
+    //มี
+    $resultInfo = array();
+
+    //Extract ข้อมูลที่ได้มาจากคำสั่ง SQL เก็บในตัวแปร
+    while ($resultData = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($resultData);
+        //สร้างตัวแปรอาร์เรย์เก็บข้อมูล
+        $resultArray = array(
+            "message" => "1",
+            "foodId" => strval($foodId),
+            "foodShopname" => $foodShopname,
+            "foodMeal" => strval($foodMeal),
+            "foodImage" => $foodImage,
+            "foodPay" => strval($foodPay),
+            "foodDate" => $foodDate,
+            "foodProvince" => $foodProvince,
+            "foodLat" => strval($foodLat),
+            "foodLng" => strval($foodLng),
+            "memId" => strval($memId)
+        );
+        array_push($resultInfo, $resultArray);
+    }
+
+
+    echo json_encode($resultInfo, JSON_UNESCAPED_UNICODE);
+} else {
+    $resultInfo = array();
+    $resultArray = array(
+        "message" => "0"
+    );
+    array_push($resultInfo, $resultArray);
+    echo json_encode(array("message" => "0"));
+}
---
+++ models/member.php
@@ -0,0 +1,81 @@
+<?php
+
+class Member
+{
+    //ตัวแปรที่ใช้เก็บการติดต่อฐานข้อมูล
+    private $connDB;
+
+    //ตัวแปรที่ทำงานคู่กับคอลัมน์(ฟิวล์)ในตาราง
+    public $memid;
+    public $memFullname;
+    public $memEmail;
+    public $memUsername;
+    public $memPassword;
+    public $memAge;
+
+    //ตัวแปรสารพัดประโยชน์
+    public $message;
+
+    //constructor
+    public function __construct($connDB)
+    {
+        $this->connDB = $connDB;
+    }
+    //----------------------------------------------
+    //ฟังก์ชันการทำงานที่ล้อกับส่วนของ APIs
+
+    //ฟังชันก์ตรวจสอบชื่อผู้ใช้และรหัสผ่าน
+    public function checkLogin()
+    {
+        //ตัวแปรเก็บคำสั่ง SQL
+        $strSQL = "SELECT * FROM member_tb WHERE memUsername = :memUsername AND memPassword = :memPassword";
+
+        //ตรวจสอบค่าที่ถูกส่งจาก Client/User ก่อนที่จะกำหนดให้กับ parameters (:????)
+        $this->memUsername = htmlspecialchars(strip_tags($this->memUsername));
+        $this->memPassword = htmlspecialchars(strip_tags($this->memPassword));
+
+        //สร้างตัวแปรที่ใช้ทำงานกับคำสั่ง SQL
+        $stmt = $this->connDB->prepare($strSQL);
+
+        //เอาที่ผ่านการตรวจแล้วไปกำหนดให้กับ parameters
+        $stmt->bindParam(":memUsername", $this->memUsername);
+        $stmt->bindParam(":memPassword", $this->memPassword);
+
+        //สั่งให้ SQL ทำงาน
+        $stmt->execute();
+
+        //ส่งค่าผลการทำงานกลับไปยังจุดเรียกใช้ฟังก์ชันนี้
+        return $stmt;
+    }
+
+    //ฟังก์ชันเพิ่มข้อมูลผู้ใช้ใหม่
+    public function registerMember()
+    {
+        //ตัวแปรเก็บคำสั่ง SQL
+        $strSQL = "INSERT INTO member_tb (`memFullname`, `memEmail`, `memUsername`, `memPassword`, `memAge` ) VALUES (:memFullname, :memEmail, :memUsername, :memPassword, :memAge);";
+
+        //ตรวจสอบค่าที่ถูกส่งจาก Client/User ก่อนที่จะกำหนดให้กับ parameters (:????)
+        $this->memFullname = htmlspecialchars(strip_tags($this->memFullname));
+        $this->memEmail = htmlspecialchars(strip_tags($this->memEmail));
+        $this->memUsername = htmlspecialchars(strip_tags($this->memUsername));
+        $this->memPassword = htmlspecialchars(strip_tags($this->memPassword));
+        $this->memAge = intval(htmlspecialchars(strip_tags($this->memAge)));
+
+        //สร้างตัวแปรที่ใช้ทำงานกับคำสั่ง SQL
+        $stmt = $this->connDB->prepare($strSQL);
+
+        //เอาที่ผ่านการตรวจแล้วไปกำหนดให้กับ parameters
+        $stmt->bindParam(":memFullname", $this->memFullname);
+        $stmt->bindParam(":memEmail", $this->memEmail);
+        $stmt->bindParam(":memUsername", $this->memUsername);
+        $stmt->bindParam(":memPassword", $this->memPassword);
+        $stmt->bindParam(":memAge", $this->memAge);
+
+        //สั่งให้ SQL ทำงาน และส่งผลลัพธ์ว่าเพิ่มข้อมูลสําเร็จหรือไม่
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
